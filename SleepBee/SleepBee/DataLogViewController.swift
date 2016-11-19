0bdfd13ece1efb428aba690228801f1fdaed3085
alexistran <alexistran@berkeley.edu>
2016-11-19 13:37:17 -0800

    sleepbee app

---
+++ SleepBee/SleepBee/DataLogViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SleepBee
+//
+//  Created by Alexis Tran on 11/19/16.
+//  Copyright © 2016 Alexis Tran. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
