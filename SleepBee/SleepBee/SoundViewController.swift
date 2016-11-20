b11c7185c1f55424ac75f100d71e49c7578e5ffe
Jasmine <jasminelee@berkeley.edu>
2016-11-19 17:16:24 -0800

    Add default alarm clock sound to list of alarm sounds - Change SoundView to SoundVietController on storyboard - Add setting to plist that allows mp3 downloads

--- SleepBee/SleepBee/SoundViewController.swift
+++ SleepBee/SleepBee/SoundViewController.swift
@@ -2,20 +2,57 @@
 //  SoundViewController.swift
 //  SleepBee
 //
-//  Created by Alexis Tran on 11/19/16.
-//  Copyright © 2016 Alexis Tran. All rights reserved.
+//  Created by Jasmine Lee on 11/19/16.
+//  Copyright © 2016 Jasmine Lee. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class SoundViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var paused = true
+    var alarmAudioPlayer: AVQueuePlayer!
 
-        // Do any additional setup after loading the view.
+    @IBAction func Radar(_ sender: UIButton) { // deselect to pause
+        if paused {
+            let radar = "http://mobiringtones.net/uploads/ringtones/1679499-Radar.mp3"
+            playSound(trackURL: radar)
+        } else {
+            alarmAudioPlayer.pause()
+        }
+        paused = !paused
+        sender.isSelected = !sender.isSelected
     }
 
+    @IBAction func Marimba(_ sender: UIButton) {
+        if paused {
+            let marimba = ""
+            playSound(trackURL: marimba)
+        } else {
+            alarmAudioPlayer.pause()
+        }
+        paused = !paused
+        sender.isSelected = !sender.isSelected
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        alarmAudioPlayer = AVQueuePlayer()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func playSound(trackURL: String) {
+        let url = URL(string: trackURL)
+        let item:AVPlayerItem = AVPlayerItem(url: url!)
+        alarmAudioPlayer.replaceCurrentItem(with: item)
+        alarmAudioPlayer!.play()
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
