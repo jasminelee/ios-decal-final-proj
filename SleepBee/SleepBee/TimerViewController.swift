97b6186d31f71e05708792e61484e2f5d6baba61
Jasmine <jasminelee@berkeley.edu>
2016-11-30 21:16:27 -0800

    Add elementary Timer code

--- SleepBee/SleepBee/TimerViewController.swift
+++ SleepBee/SleepBee/TimerViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
+
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBAction func startTimer(_ sender: UIButton) {
+        timerLabel.text = "Timer Started"
+        timer = Timer.scheduledTimer(timeInterval: timeInterval,                                                       target: self,                                                       selector: Selector(("timerDidEnd:")),                                                       userInfo: "Bzz bzz! Time to wake up!",                                                       repeats: false)
+    }
+    
+    @IBAction func stopTimer(_ sender: UIButton) {
+        timerLabel.text = "Timer Stopped"
+        timer.invalidate()
+    }
+    
+    @IBAction func resetTimer(_ sender: UIButton) {
+    }
+    
+    var timer = Timer() //make a timer variable, but don't do anything yet
+    let timeInterval:TimeInterval = 10.0
+    
+    //MARK: - Instance Methods
+    func timerDidEnd(timer:Timer){
+        //first iteration of timer
+        timerLabel.text = timer.userInfo as? String
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
