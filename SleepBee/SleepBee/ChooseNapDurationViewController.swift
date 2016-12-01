e1991c13cc3b4314ff7696b2b97a3ce107125de4
Alexis <alexistran@berkeley.edu>
2016-11-30 21:59:45 -0800

    pls work

--- SleepBee/SleepBee/ChooseNapDurationViewController.swift
+++ SleepBee/SleepBee/ChooseNapDurationViewController.swift
@@ -9,14 +9,11 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        self.view.backgroundColor = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
